95cb89316415e01b7486c3385893638d31a412e8
arshivam <shivam.armarkar007@gmail.com>
2022-02-26 18:15:41 +0530

    Initial commit

---
+++ test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.moodanalyser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MoodAnalyserTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
